a3840a2a07f37b44ff7f4355ced96547d383d024
Smriti Doneria <smritidoneria@gmail.com>
2024-05-17 16:21:35 +0530

    vercel configurations

--- db.js
+++ db.js
@@ -2,8 +2,8 @@ require('dotenv').config();
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-});
+    connectionString: process.env.POSTGRES_URL,
+  })
 
 pool.connect((err, client, release) => {
   if (err) {
